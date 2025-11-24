c0f8a22eff89005f217eedea6abc251b5079ab2f
Sai Charan Gottipalli <saicharangottipalli@gmail.com>
2025-11-24 14:16:36 +0530

    Create InsertionSort.c

---
+++ InsertionSort.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+	int n;
+	scanf("%d",&n);
+	int a[n];
+	for(int i=0;i<n;i++) scanf("%d",&a[i]);
+	for(int i=1;i<n;i++){
+		int key=a[i];
+		int j=i-1;
+		while(j>=0&&key<a[j]){
+			a[j+1]=a[j];
+			j--;
+		}
+		a[j+1]=key;
+	}
+	for(int i=0;i<n;i++) printf("%d ",a[i]);
+}
